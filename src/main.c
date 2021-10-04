21608d48bdf2d5b9c579bfe96b20217c78f4595e
Michael Bittencourt <mabm@cesar.org.br>
2021-10-04 17:07:10 -0400

    Made changes to execute more parallel

--- src/main.c
+++ src/main.c
@@ -10,9 +10,17 @@ int main(void) {
 
     pthread_t threads[QTD_THREADS];
     int retCreation[QTD_THREADS];
+    int params[QTD_THREADS];
     for(int i = 0; i < QTD_THREADS; i++) {
-        retCreation[i] = pthread_create( &(threads[i]), NULL, printThread, (void*)&i);
+        params[i] = i;
+        retCreation[i] = pthread_create( &(threads[i]), NULL, printThread, (void*)(&(params[i])));
+    }
+
+    for(int i = 0; i < QTD_THREADS; i++) {
         pthread_join(threads[i], NULL);
+    }
+
+    for(int i = 0; i < QTD_THREADS; i++) {
         printf("Ret of thread %d creation: %d\n", i, retCreation[i]);
     }
 
